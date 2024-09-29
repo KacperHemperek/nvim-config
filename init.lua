54990bc26bb8421162e49e522fad4623b9b4b8af
Kacper Hemperek <kacper.hemperek@apptimia.com>
2024-09-29 10:33:22 +0200

    add copilot

--- init.lua
+++ init.lua
@@ -189,6 +189,8 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<leader>x', '<cmd>wa|%bd|e#<CR>', { desc = 'Save and remove all buffers besides the one that is being edited' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
