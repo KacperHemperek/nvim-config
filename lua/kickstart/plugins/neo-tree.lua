559c40b3d8cc3fa45d7a29d9ea2e2f984bdd106b
Kacper Hemperek <kacper.hemperek@apptimia.com>
2024-09-29 10:35:59 +0200

    show hidden and git ignored files in neo-tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -25,6 +25,11 @@ return {
       },
     },
     filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
