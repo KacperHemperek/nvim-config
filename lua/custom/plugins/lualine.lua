6c089e9facbaebd417eaa6abafaf48fb5fbd64fb
Kacper Hemperek <kacper.hemperek@o2.pl>
2025-10-13 11:28:07 +0200

    update lualine deviders

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -15,15 +15,15 @@ return {
         S = 'SELECT',
         t = 'TERMINAL',
       }
-      return ' ' .. mapping[m]
+      return '  ' .. mapping[m]
     end
 
     require('lualine').setup {
       options = {
         icons_enabled = true,
         theme = 'auto',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
         disabled_filetypes = {
           statusline = {},
           winbar = {},
